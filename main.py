77c00bd8a081bda5d2dbc6ec42dc6a2c4a77022a
skAdOOdlEs7 <grantdriddle7@gmailcom>
2022-02-12 23:31:41 -0700

    partially working powers and working division

--- main.py
+++ main.py
@@ -24,23 +24,16 @@ class Lambda:
         return self.greg(input)
 
 
-thing = input("eee: ")
-carl = ""
-for i in thing:
-    if i != "." and i != " ":
-        carl +=i
-
-
 def split_parentheses(strang):
     output = []
     last = 0
-    last_closed = 0
+    last_closed = -1
     layers = 0
     current = 0
     opened = False
 
     complete = True
-    
+
     for i in range(len(strang)):
         for j in range(len(strang[i])):
             if strang[i][j] == "(":
@@ -50,9 +43,8 @@ def split_parentheses(strang):
                 if layers == 0:
                     last = current
 
-                    temp = strang[i][last:j]
-                    print(temp)
-                    if temp and "(" not in temp:
+                    temp = strang[i][last_closed + 1:j]
+                    if temp and ")" not in temp:
                         output.append(temp)
 
                     current = j
@@ -70,7 +62,7 @@ def split_parentheses(strang):
 
         if last_closed != j:
             output.append(strang[i][last_closed + 1:])
-        if last_closed == 0:
+        if last_closed == -1:
             return strang
 
     if not complete:
@@ -81,4 +73,55 @@ def split_parentheses(strang):
     return output
 
 
-print(split_parentheses([carl]))
+def format_string(stringo):
+    temp = ""
+    for i in stringo:
+        if i != " ":
+            temp += i
+    return temp
+
+
+def divide_units(listoid, above = True, layers = 0):
+    top = []
+    bottom = []
+    layers += 1
+    if type(listoid) == list and listoid:
+        for i in listoid:
+            temp = divide_units(i, above)
+            top += temp[0]
+            bottom += temp[1]
+            above = temp[2]
+
+    elif type(listoid) == str:
+        for i in listoid:
+            if i == "/":
+                above = not above
+            elif above:
+                top.append(i)
+            else:
+                bottom.append(i)
+
+    return [top, bottom, above]
+
+
+def pow_units(strang):
+    previous = 0
+    temp = ""
+
+    for i in range(len(strang)):
+        if i + 2 < len(strang) and strang[i:i+2] == "**":
+            temp += strang[previous:i-1] + strang[i-1] * int(strang[i+2])
+            previous = i
+
+    return temp
+
+
+def get_units(inp):
+    inp = format_string(inp)
+    inp = pow_units(inp)
+
+
+thing = input("eee: ")
+
+carl = pow_units(thing)
+print(carl)
